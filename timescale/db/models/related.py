a06b37e4cb349d8d145ca36769b5eaa7e2844ed4
somers <jake.somerville@lynxx.com>
2024-10-16 17:39:53 +1100

    + override for get_prefetch_querysets to always prefetch using time field for TimescaleRelatedForeignKeys

--- timescale/db/models/related.py
+++ timescale/db/models/related.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.db.models.fields.related_descriptors import ForwardManyToOneDescriptor
+
+
+class TimescaleRelatedForwardManyToOneDescriptor(ForwardManyToOneDescriptor):
+    def get_prefetch_querysets(self, instances, queryset=None):
+        queryset = queryset or self.get_queryset()
+        times = set(instance.time for instance in instances)
+        queryset = queryset.filter(time__in=times)
+        return super().get_prefetch_querysets(instances, [queryset])
 
 
 class TimescaleRelatedForeignKey(models.ForeignKey):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super().contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, TimescaleRelatedForwardManyToOneDescriptor(self))
+
     def resolve_related_fields(self):
         related_fields = super().resolve_related_fields()
         related_fields.append((self.opts.get_field('time'), self.remote_field.model._meta.get_field('time')))
